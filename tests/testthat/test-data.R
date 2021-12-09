79fd09e7ea9eded402e9d05c02e90afc59e30e62
Sebastian Toro Arana <seba@DN2texib.SUNet>
2021-12-09 10:45:59 -0800

    Created tests for data

---
+++ tests/testthat/test-data.R
@@ -0,0 +1,64 @@
+test_that("check number of rows", {
+  expect_true(nrow(buttR_neuro) == 414)
+})
+
+test_that("data has correct columns", {
+  expect_true("count" %in% colnames(buttR_neuro))
+  expect_true("gene" %in% colnames(buttR_neuro))
+  expect_true("sample" %in% colnames(buttR_neuro))
+  expect_true("individual" %in% colnames(buttR_neuro))
+  expect_true("sex" %in% colnames(buttR_neuro))
+  expect_true("species" %in% colnames(buttR_neuro))
+  expect_true("social" %in% colnames(buttR_neuro))
+  expect_true("region_mammal" %in% colnames(buttR_neuro))
+})
+
+test_that("columns have correct data type", {
+  expect_true(typeof(buttR_neuro$count) == "double")
+  expect_true(typeof(buttR_neuro$gene) == "integer")
+  expect_true(typeof(buttR_neuro$sample) == "integer")
+  expect_true(typeof(buttR_neuro$individual) == "integer")
+  expect_true(typeof(buttR_neuro$sex) == "integer")
+  expect_true(typeof(buttR_neuro$species) == "integer")
+  expect_true(typeof(buttR_neuro$social) == "integer")
+  expect_true(typeof(buttR_neuro$region_mammal) == "integer")
+})
+
+test_that("columns have correct class", {
+  expect_true(class(buttR_neuro$count) == "numeric")
+  expect_true(class(buttR_neuro$gene) == "factor")
+  expect_true(class(buttR_neuro$sample) == "integer")
+  expect_true(class(buttR_neuro$individual) == "factor")
+  expect_true(class(buttR_neuro$sex) == "factor")
+  expect_true(class(buttR_neuro$species) == "factor")
+  expect_true(class(buttR_neuro$social) == "factor")
+  expect_true(class(buttR_neuro$region_mammal) == "factor")
+})
+
+test_that("gene column has correct control", {
+  expect_true("r18S" %in% buttR_neuro$gene)
+})
+
+test_that("all sexes are male", {
+  expect_true(length(unique(buttR_neuro$sex))==1)
+  expect_true(unique(buttR_neuro$sex)[1]=="Male")
+})
+
+test_that("all species are there", {
+  expect_true("C.bar" %in% buttR_neuro$species)
+  expect_true("C.lun" %in% buttR_neuro$species)
+  expect_true("C.rainf" %in% buttR_neuro$species)
+  expect_true("C.trif" %in% buttR_neuro$species)
+  expect_true("C.vag" %in% buttR_neuro$species)
+})
+
+test_that("all brain regions are meAMY.BNST", {
+  expect_true(length(unique(buttR_neuro$region_mammal))==1)
+  expect_true(unique(buttR_neuro$region_mammal)[1]=="meAMY.BNST")
+})
+
+#have a test set for inputs and outputs to functions
+# -right inputs
+# -make sure packages are running well
+# -make sure models are running well
+# -right outputs
